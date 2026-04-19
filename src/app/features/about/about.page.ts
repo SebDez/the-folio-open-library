1c65386d069b9050bd6e2cac28aaf9d02ab919fc
SebDez <sebdez.dev@gmail.com>
2026-04-19 13:10:01 +0200

    fix(text): fix about page text

--- src/app/features/about/about.page.ts
+++ src/app/features/about/about.page.ts
@@ -1,21 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 import { AuthorModel } from '../../core/authors/models/author.model';
 import { AuthorDisplayComponent } from '../author/components/author-display/author-display.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [AuthorDisplayComponent],
+  imports: [AuthorDisplayComponent, TranslateModule],
   templateUrl: './about.page.html',
 })
 export class AboutPage {
-  protected readonly currentAuthor: AuthorModel = {
+  readonly currentAuthor = signal<AuthorModel>({
     id: 'about-bio',
-    name: 'Sébastien Dez',
+    name: 'about.name',
     birthDate: '1991-07-25',
-    bio: "Développeur Fullstack JavaScript avec 12 ans d'expérience, j'allie la rigueur du Clean Code à une approche humaine du mentoring (scrum master, accompagnement juniors). Mon expertise (Vue.js, NestJS, Angular, Nuxt) est au service de la création de solutions durables et du partage de connaissances au sein des équipes. J’applique une approche d’Artisan Développeur basée sur la \"Bienvaillance\" : l’équilibre entre la bienveillance humaine (mentoring, écoute utilisateur) et la vigilance technique (rigueur, maintenance, performance). Mon objectif : délivrer de la valeur métier durable au travers d’un code propre et d’une culture d'amélioration continue.",
-  };
+    bio: 'about.bio',
+  });
 
-  protected readonly photoUrl = 'https://avatars.githubusercontent.com/u/10667575?v=4';
+  protected readonly photoUrl =
+    'https://media.licdn.com/dms/image/v2/D4D03AQGmGyS_3Cq90g/profile-displayphoto-crop_800_800/B4DZ1KLPzyHgAI-/0/1775065961341?e=1778112000&v=beta&t=E926P2Cl3MIofnKFut9-oTJ596Zt_m4BWSrZnrfbx_g';
+
+  constructor(private readonly translate: TranslateService) {
+    this.updateCurrentAuthor();
+    this.translate.onLangChange.pipe(takeUntilDestroyed()).subscribe(() => {
+      this.updateCurrentAuthor();
+    });
+  }
+
+  private updateCurrentAuthor(): void {
+    this.currentAuthor.set({
+      id: 'about-bio',
+      name: this.translate.instant('about.name'),
+      birthDate: '1991-07-25',
+      bio: this.translate.instant('about.bio'),
+    });
+  }
 }
